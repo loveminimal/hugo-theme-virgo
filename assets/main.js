ffd7148ba25b29a28e2cbb85308bc13060c8bc6a
jack <loveminimal@163.com>
2022-07-06 09:47:43 +0800

    Remodule js.

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,7 @@ import initEventBinding from "js/init/eventBinding";
 import initToc from "js/init/initToc";
 import initCodeBlock from "js/init/initCodeBlock";
 import initMouseClickAnimate from "js/init/initMouseClickAnimate";
-import enhanceOrgMode from 'js/utils/enhanceOrgMode';
+import enhanceOrgMode from './js/init/enhanceOrgMode';
 
 greet();
 initEventBinding();
