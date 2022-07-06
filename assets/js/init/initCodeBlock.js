002453316a2b6bb1bbdb64e6c8bd610aca4cb4fe
jack <loveminimal@163.com>
2022-07-06 11:28:45 +0800

    Config fold code blocks.

--- assets/js/init/initCodeBlock.js
+++ assets/js/init/initCodeBlock.js
@@ -1,15 +1,19 @@
 import $ from '../libs/jquery.min';
 import initVars from 'js/init/initVars';
+import isMobile from 'js/utils/isMobile';
 
 let varsDict = initVars();
 
 export default function initCodeBlock() {
-	console.log(varsDict.hasFoldAllCodeBlocks);
+	// console.log(varsDict.hasFoldAllCodeBlocks);
 	const _arr = $('.highlight pre code');
 
 	_arr.each(function(idx) {
 		// let isShow = true;	// 初始化是否显示代码块
 		let isShow = !varsDict.hasFoldAllCodeBlocks;
+
+		// 移动端全部默认折叠代码块
+		if (isMobile()) isShow = false;
 		
 		let _this = $(this);
 		let _lang = $(this).attr('data-lang');
--- assets/js/init/initVars.js
+++ assets/js/init/initVars.js
@@ -2,7 +2,7 @@ import $ from '../libs/jquery.min';
 
 export default function initVars() {
 	let _vars = $('#vars');
-	console.log(_vars.text());
+	// console.log(_vars.text());
 
 	return JSON.parse(_vars.text())
 }
