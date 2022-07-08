a42649132318cb33f2989b053aeda1cabb491369
Jack Liu <loveminimal@163.com>
2022-07-08 21:26:37 +0800

    Add a sun icon and init color.

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,13 @@
 import greet from "js/init/greet";
 import initEventBinding from "js/init/eventBinding";
+import {initColor} from "js/utils/toggleColor";
 import initCodeBlock from "js/init/initCodeBlock";
 import initMouseClickAnimate from "js/init/initMouseClickAnimate";
 import enhanceOrgMode from './js/init/enhanceOrgMode';
 
 greet();
 initEventBinding();
+initColor();
 initCodeBlock();
 // initMouseClickAnimate();
 enhanceOrgMode();
