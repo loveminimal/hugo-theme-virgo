fa4c7757b711ca4b58ecb974d33d8cf74625e907
jack <loveminimal@163.com>
2022-07-26 14:44:53 +0800

    Enhanced pre as a parallel panels.

--- assets/js/init/initCodeBlock.js
+++ assets/js/init/initCodeBlock.js
@@ -3,25 +3,35 @@ import parseSiteParams from '../utils/parseSiteParams';
 import isMobile from 'js/utils/isMobile';
 
 export default function initCodeBlock() {
-	const _arr = $('.highlight pre code');
+	// const _arr = $('.highlight pre code');
+	const _arr = $('pre code');
 
-	_arr.each(function(idx) {
+	_arr.each(function (idx) {
 		// let isShow = true;	// 初始化是否显示代码块
 		let isShow = !parseSiteParams().hasFoldAllCodeBlocks;
 
 		// 移动端全部默认折叠代码块
 		if (isMobile()) isShow = false;
-		
+
 		let _this = $(this);
 		let _lang = $(this).attr('data-lang');
 		let _id = _lang + idx;
-		
+
+
+		// 模拟一个折叠板的功能，当
+		// _lang 为 'fold' 时，默认为折叠板
+		if (!_lang) return;
+		if (_lang === 'fold') {
+			_lang = '';
+			isShow = false;
+		}
+
 		if (!isShow) _this.css('display', 'none');
-		
+
 		$(this).before(`<div class="lang" id="${_id}">${isShow ? _lang + ' ▽' : _lang + ' ▷'}</div>`)
 		let _langEle = $('#' + _id);
 
-		_langEle.click(function() {
+		_langEle.click(function () {
 			_this.toggle();
 			isShow = !isShow;
 			_langEle.html(isShow ? `${_lang} ▽` : `${_lang} ▷`)
