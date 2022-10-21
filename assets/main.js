80a39598800c5ba60c74fe43fc39cdf596b718ba
jack <loveminimal@163.com>
2022-10-21 10:23:56 +0800

    Powerful nav page.

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,14 @@
 import greet from "js/init/greet";
 import initEventBinding from "js/init/eventBinding";
 import initCodeBlock from "js/init/initCodeBlock";
+import initNavLink from "js/init/initNavLink";
 import initMouseClickAnimate from "js/init/initMouseClickAnimate";
 import enhanceOrgMode from './js/init/enhanceOrgMode';
 import enhanceMarkdown from './js/init/enhanceMarkdown';
 
 greet();
 enhanceOrgMode();
+initNavLink();
 initEventBinding();
 initCodeBlock();
 enhanceMarkdown();
