7a6798bbd97c4647db222de05cd2288b31f37f9b
jack <loveminimal@163.com>
2022-10-21 11:42:21 +0800

    Better code showwing.

--- assets/js/init/enhanceMarkdown.js
+++ assets/js/init/enhanceMarkdown.js
@@ -2,9 +2,9 @@ import $ from 'js/libs/jquery.min';
 
 export default function() {
 	$('em').each((idx, item) => {
-		console.log(item.outerHTML)
+		// console.log(item.outerHTML)
 
-		let _innerText = item.innerText;
+		let _innerText = item.innerHTML;
 
 		if (_innerText.indexOf('_') === 0) {
 			// ^ *_xxx* 转译为 <u>xxx</u>
@@ -13,6 +13,7 @@ export default function() {
 		} else if (_innerText.indexOf('-') === 0) {
 			// ^ *-xxx* 转译为 <span class="oh-hl">xxx</span>
 			item.outerHTML = `<span class="oh-hl">${_innerText.slice(1)}</span>`
+			console.log($('.oh-hl')[idx]);
 
 		} else if (_innerText.indexOf('=') === 0) {
 			// ^ *=xxx* 转译为 <div class="oh-essay">xxx</div>
