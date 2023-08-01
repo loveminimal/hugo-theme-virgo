408237bc6427c4fb368ad3846d95214d8757b907
jack <loveminimal@163.com>
2023-08-01 16:29:15 +0800

    Nav.

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,9 @@
 import greet from "js/init/greet";
 import initEventBinding from "js/init/eventBinding";
-import initNavLink from "js/init/initNavLink";
 import enhanceOrgMode from './js/init/enhanceOrgMode';
 import enhanceMarkdown from './js/init/enhanceMarkdown';
 
 greet();
 enhanceOrgMode();
-initNavLink();
 enhanceMarkdown();
 initEventBinding();
