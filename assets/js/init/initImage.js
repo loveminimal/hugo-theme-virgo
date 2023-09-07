9d2c58cca6462b46ed2da44237f1baad4939da18
jack <loveminimal@163.com>
2023-09-07 16:33:38 +0800

    Render imgs without / .

---
+++ assets/js/init/initImage.js
@@ -0,0 +1,23 @@
+import $ from '../libs/jquery.min';
+// 在使用 Obsidian 或 Typora 自动插入图片时，
+// 自动插入的图片路径为 assets/xxx.jpg 等，而
+// Hugo 无法正确渲染访路径，它在解析的时候，会
+// 生成有类似如下路径：
+// http://localhost:1313/temp-%E8%B0%83%E8%AF%95%E6%96%87%E4%BB%B6/assets/baby.png ，所以
+// 我们对其遍历，为不以 / 开头的路径，添加 / 
+export default function initImage() {
+	$('img').each((idx, item) => {
+		// console.log(item);
+		// console.log($(item).attr('src'));
+		// console.log(item.src);
+		let src = $(item).attr('src');
+		// console.log(src);
+
+		if (!src.startsWith('/')) {
+			// console.log('!!!!!!!!!');
+			$(item).attr('src', '/' + src);
+		}
+
+	})
+}
+
--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,12 @@
 import greet from "js/init/greet";
 import initEventBinding from "js/init/eventBinding";
+import initImage from "js/init/initImage";
 import initCodeBlock from "js/init/initCodeBlock";
 import enhanceOrgMode from './js/init/enhanceOrgMode';
 import enhanceMarkdown from './js/init/enhanceMarkdown';
 
 greet();
+initImage();
 enhanceOrgMode();
 initCodeBlock();
 enhanceMarkdown();
