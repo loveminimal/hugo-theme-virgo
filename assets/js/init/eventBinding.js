7500e2b6131de9f7bb47ce0b678de7dc462eb6dd
jack <loveminimal@163.com>
2023-09-21 15:37:33 +0800

    Recovery dark mode.

--- assets/js/init/eventBinding.js
+++ assets/js/init/eventBinding.js
@@ -3,12 +3,14 @@ import $ from '../libs/jquery.min';
 import { throttle } from '../libs/lodash.min';
 import colorToc from './colorToc';
 import toggleToc from 'js/utils/toggleToc';
+import toggleColor from "../utils/toggleColor";
 import viewImage from "../utils/viewImage";
 
 
 export default function initEventBinding() {
 	$(window).bind('scroll', throttle(colorToc, 500));
 	$('#toc').bind('click', toggleToc);
+	$('#light-dark').bind('click', toggleColor);
 	if (location.href.indexOf('walkssi') > -1) $('#s').hide();
 	$('.content img').bind('click', viewImage);
 }
