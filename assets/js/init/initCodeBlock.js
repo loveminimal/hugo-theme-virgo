acabaf3db0bdf4a3dec3b59311d71ef5b50c5d9e
jack <loveminimal@163.com>
2024-12-03 15:10:00 +0800

    feat: add folding plate feature

--- assets/js/init/initCodeBlock.js
+++ assets/js/init/initCodeBlock.js
@@ -9,7 +9,33 @@ export default function initCodeBlock() {
 
 		if (!_lang) return;
 
-		// $(this).parents('.highlight').before(`<div class="lang">${!isMobile() ? _lang : '✡︎'}</div>`) //  ✡︎
-		$(this).parents('.highlight').before(`<div class="lang">${_lang}</div>`) //  ✡︎
+		if (!_lang.startsWith('_')) {
+			// ^ 非 _ 开头的代码块，在代码块前增加语言标签
+			// $(this).parents('.highlight').before(`<div class="lang">${!isMobile() ? _lang : '✡︎'}</div>`) //  ✡︎
+			$(this).parents('pre').before(`<div class="lang">${_lang}</div>`) //  ✡︎
+
+		} else {
+			_lang = _lang.slice(1);
+
+			if (!_lang.startsWith('_')) {
+				// ^ 非 __ 开头的代码块，在代码块前增加一个折叠标签（代码块样式）
+				$(this).parents('pre').before(`
+					<details>
+						<summary>${_lang}</summary>
+						<pre>${$(this).html()}</pre>
+					</details>
+				`)
+			} else {
+				// ^ 以 __ 开头的代码块，在代码块前增加一个折叠标签（引用样式）
+				$(this).parents('pre').before(`
+					<details>
+						<summary>${_lang.slice(1)}</summary>
+						<blockquote>${$(this).html()}</blockquote>
+					</details>
+				`)
+			}
+
+			$(this).parents('pre').remove();
+		}
 	})
 }
